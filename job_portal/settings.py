f1503befde0c3dfcd49fffe692fde834a7047149
navadeep <navadeep@navadeeps-MacBook-Air.local>
2026-05-12 12:29:01 +0530

    Implemented registration API and custom user model

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fysls4)((+5sh9^&)8r#61!&%dmc%j-y9%5gv-x^-_04ohzn*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
